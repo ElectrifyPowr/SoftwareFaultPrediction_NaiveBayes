6c5addb7048db3e1cebe7c74b8aa558148d6d1cc
Patrick <yourpowrelectrifyfaq@gmail.com>
2019-01-01 00:02:33 +0100

    Added javadoc

--- model/SFPModel.java
+++ model/SFPModel.java
@@ -6,7 +6,9 @@
   * @Author: ElectriyPowr
   * 
   * 
-  * SFP model
+  * SFP model - learns from dataset.txt and then makes predictions for unknown.txt
+  *
+  * Using Naive Bayes Classifier approach
   * 
   */
 package model;
@@ -21,49 +23,63 @@ import java.util.*;
 
 public class SFPModel {
 
+    // ---- constants ----
+    // possible categories for predicting, either faulty or non-faulty
     final int CATEGORY_NON_FAULTY = 0;
     final int CATEGORY_FAULTY = 0;
-
+    // possible datasets -> training or testing
     final int SET_TYPE_DATASET = 0;
     final int SET_TYPE_TESTSET = 1;
+    // used to split features of txt file
+    final String COMMA_DELIMITER = ",";
 
+    // will hold contents of dataset.txt
     double[][] dataset;
+    // will hold contents of unknown.txt
+    double[][] testSet;
+
+    // training dataset will be separated into these 2 datasets (needed for Naive Bayes)
+    double[][] faultyData;
+    double[][] nonFaultyData;
     
     // dataset of NASA CM1
     //int noOfFeatures = 21;
     //int noOfModules = 496;
     
     // custom test dataset
-    int noOfFeatures = 3;
-    int noOfTrainingModules = 5;
+    // number of features for classification
+    int noOfFeatures = 0;
+    // each training module is 1 line in dataset.txt
+    int noOfTrainingModules = 0;
+    // each testing module is 1 line unknown.txt
     int noOfTestingModules = 0;
+
     String filenameDataset = "dataset.txt";
     String filenameUnknownModules = "unknown.txt";
+    // path to both text files
     String path = "./";
 
-    double[][] faultyData;
-    double[][] nonFaultyData;
-
-    private static final String COMMA_DELIMITER = ",";
-
+    // some testing data for NASA CM1 dataset
     /*double[][] testSet = new double[][]{
         {12,3,1,1,51,227.43,0.1,10.42,21.83,2369.07,0.08,131.62,0,0,1,0,10,12,26,25,5,0},
         {31,4,1,2,141,829.45,0.05,21.52,38.55,17846.19,0.28,991.46,1,19,15,0,27,32,90,51,7,1},
         {28,6,5,5,104,564.33,0.06,16.09,35.08,9078.38,0.19,504.35,2,7,0,0,20,23,67,37,11,1}
     };*/
 
-    double[][] testSet;
-    
+    /**
+     * Main Function - Starts prediction model
+     *
+     */
     public static void main(String[] args) {
         SFPModel model = new SFPModel();
         model.start();
     }
 
+    /**
+     * Loads datasets and makes predictions.
+     *
+     */
     public void start(){
-        //int dsCount = getNumberOfLinesInFile(nameDataset);
-        //int testCount = getNumberOfLinesInFile(nameUnknownModules);
-        //System.out.println("dsCount: "+dsCount+", testCount: "+testCount);
-
         //load training data
         loadDataset();
         separateDataset();
@@ -71,11 +87,13 @@ public class SFPModel {
         // load test data
         loadUnknownModules();
 
-        System.out.println("noOfFeatures: "+noOfFeatures+", noOfTrainingModules: "+noOfTrainingModules+", noOfTestingModules: "+noOfTestingModules);
-
         makePredictions();
     }
 
+    /**
+     * Iterates through all testingModules and makes predictions whether they are faulty or non-faulty.
+     *
+     */
     public void makePredictions(){
         System.out.println("Starting prediction...");
         
@@ -84,15 +102,21 @@ public class SFPModel {
             naiveBayesClassifier(testSet[set]);
             System.out.println("-------------------------------------------------------------------------------");
         }
-
+        System.out.println("End of predictions...");
     }
 
+    /**
+     * Use Naive Bayes Classifier to make prediction on a testing module.
+     *
+     * @param moduleUnderTest - holds all features of a module that should be predicted
+     *
+     */
     private void naiveBayesClassifier(double[] moduleUnderTest){
         double[][] nonFaultyFeatureData = processNonFaultyData();
         double[][] faultyFeatureData = processFaultyData();
 
         //calc non-faulty probability
-        // lastly, multiply by p(C=c) = p(C=No) = p(C=0)
+        // firstly, assign p(C=c) = p(C=No) = p(C=0)
         double nonFaultTmp = getCategorySpecificSetSize(CATEGORY_NON_FAULTY);
         for(int feature=0; feature<moduleUnderTest.length-1; feature++){
             double x = moduleUnderTest[feature];
@@ -105,7 +129,7 @@ public class SFPModel {
         }
 
         //calc faulty probability
-        // lastly, multiply by p(C=c) = p(C=Yes) = p(C=1)
+        // firstly, assign p(C=c) = p(C=Yes) = p(C=1)
         double faultTmp = getCategorySpecificSetSize(CATEGORY_FAULTY);
         for(int feature=0; feature<moduleUnderTest.length-1; feature++){
             double x = moduleUnderTest[feature];
@@ -122,11 +146,29 @@ public class SFPModel {
         String knownCategory = getPredictedCategoryString((int)(moduleUnderTest[moduleUnderTest.length-1]));
         String predictedString = getPredictedCategoryString(predictedCategory);
 
+        // print out prediction
         System.out.println("Module is "+knownCategory+",\tPrediction: "+predictedString);
+        // print out raw values of non-faulty prediction and faulty prediction
         System.out.println("NonFaultTmp: "+nonFaultTmp+", FaultTmp: "+faultTmp);
-
     }
 
+    /**
+     * Calculates all necessary means and standard deviations for each feature of non-faulty dataset.
+     * 
+     * @return 2D-array with following structure:
+     *
+     *      | f1  |   f2  |   ...     |   fn    |
+     * -----|-----|-------|-----------|---------|
+     * mean |     |       |           |         |
+     * -----|-----|-------|-----------|---------|
+     * stDe |     |       |           |         |
+     * ------------------------------------------
+     *      
+     * Where f1, f2, ..., fn represents each feature
+     *       mean represents arithmetic mean of 1 feature of all data in dataset
+     *       stDe represents standard deviation of 1 feature of all data in dataset
+     *
+     */
     private double[][] processNonFaultyData(){
         // will contain mean & std_dev for each feature
         double[][] data = new double[noOfFeatures][2];
@@ -139,6 +181,11 @@ public class SFPModel {
         return data;
     }
     
+    /**
+     * Same as 'processNonFaultyData', but here for the faulty dataset.
+     * @return 2D-array
+     *
+     */
     private double[][] processFaultyData(){
         // will contain mean & std_dev for each feature
         double[][] data = new double[noOfFeatures][2];
@@ -151,6 +198,11 @@ public class SFPModel {
         return data;
     }
 
+    /**
+     * Separates training dataset into 2 datasets:
+     *      - Faulty dataset (which only contains faulty modules)
+     *      - Non-faulty dataset (only contains non-faulty modules)
+     */
     private void separateDataset(){
         ArrayList<Integer> faultyDataIdx = new ArrayList<>();
         ArrayList<Integer> nonFaultyDataIdx = new ArrayList<>();
@@ -178,12 +230,30 @@ public class SFPModel {
         }
     }
 
+    /**
+     * Calculates Gauss Distribution Function.
+     *
+     * @param x - value of feature
+     * @param mean - mean of all values of certain feature of training dataset
+     * @param stDev - standard deviation of all values of certain feature of training dataset
+     *
+     * @return processed value on distribution function
+     */
     public double calcGaussDistribution(double x, double mean, double stDev){
         double firstPart = 1 / Math.sqrt(2 * Math.PI * Math.pow(stDev, 2));
         double secPart = Math.exp(-Math.pow((x-mean), 2) / (2 * Math.pow(stDev, 2)));
         return (firstPart * secPart);
     }
 
+    /**
+     * Calculates standard deviation of one feature of dataset
+     *
+     * @param data - the dataset
+     * @param mean - mean of all values of certain feature of training dataset 'data'
+     * @param feature - index of feature for which stDev should be calculated for
+     *
+     * @return standard deviation
+     */
     public double standardDeviation(double[][] data, double mean, int feature){
         int n = data.length;
         double stDev = 0;
@@ -199,6 +269,13 @@ public class SFPModel {
         return stDev;
     }
 
+    /**
+     * Get the category specific set size
+     *
+     * @param category - based on this, the set size should be returned
+     *
+     * @return set size
+     */
     public double getCategorySpecificSetSize(int category){
         if(category==CATEGORY_NON_FAULTY)
             return ((double)(nonFaultyData.length) / (double)(dataset.length));
@@ -206,6 +283,14 @@ public class SFPModel {
             return ((double)(faultyData.length) / (double)(dataset.length));
     }
 
+    /**
+     * Get string representation of certain category
+     *
+     * @param category - 0 = non-faulty
+     *                 - 1 = faulty
+     *
+     * @return string representation
+     */
     public String getPredictedCategoryString(int category){
         if(category==CATEGORY_NON_FAULTY)
             return "Non-Faulty";
@@ -213,6 +298,14 @@ public class SFPModel {
             return "Faulty";
     }
 
+    /**
+     * Gets all the values of one feature in the dataset 'data'
+     *
+     * @param data - dataset
+     * @param feature 
+     *
+     * @return all values for one feature
+     */
     public double[] getValuesForMean(double[][] data, int feature){
         double[] vals = new double[data.length];
 
@@ -222,6 +315,14 @@ public class SFPModel {
         return vals;
     }
 
+    /**
+     * Calculates the arithmetic mean of a certain feature in dataset 'data'
+     *
+     * @param data - dataset
+     * @param feature
+     *
+     * @return arithmetic mean
+     */
     public double arithmeticMean(double[][] data, int feature){
         double[] values = getValuesForMean(data, feature);
         double n = (double)(values.length);
@@ -234,6 +335,13 @@ public class SFPModel {
         return (tmp/n);
     }
 
+    /**
+     * Calculates the maximum value of a given array and returns its index of the array
+     *
+     * @param vals - data array
+     *
+     * @return index of max value in array
+     */
     public int argmax(double[] vals){
         int idx = 0;
         for(int i=0; i<vals.length; i++){
@@ -244,8 +352,7 @@ public class SFPModel {
     }
 
     /**
-     * converts information from a csv file into a 2d dataset array
-     * @return
+     * Loads the training data from text file into dataset array.
      */
     public void loadDataset(){
         processLinesAndFeaturesOfFile(filenameDataset, SET_TYPE_DATASET);
@@ -279,12 +386,10 @@ public class SFPModel {
     }
     
     /**
-     * converts information from a csv file into a 2d dataset array
-     * @return
+     * Loads the test data from text file into the testSet array.
      */
     public void loadUnknownModules(){
         processLinesAndFeaturesOfFile(filenameUnknownModules, SET_TYPE_TESTSET);
-
         BufferedReader fileReader = null;
         try {
             String line = "";
@@ -314,6 +419,13 @@ public class SFPModel {
          }
     }
 
+    /**
+     * Reads the number of modules and features of a file.
+     *
+     * @param filename - can be either dataset.txt or unknown.txt
+     * @param type - helps distinguish between both sets
+     *
+     */
     private void processLinesAndFeaturesOfFile(String filename, int type){
         BufferedReader fileReader = null;
         try {
@@ -346,6 +458,8 @@ public class SFPModel {
             }
          }
     }
+
+    // ----- getters & setters -----
 
     public double[][] getDataset(){return dataset;}
 
